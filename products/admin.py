540eaa951adbc2a5875c597895da12847122a637
bryancitu <bryanjesus.ra@gmail.com>
2022-01-11 08:57:03 -0500

    admin products

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'url_image', 'price', 'discount', 'category']
+    list_filter = ['category']
+    list_editable = ['price']
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
